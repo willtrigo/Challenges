ac4dfb725ab4e050df95098f8c35120e0663f593
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-04 07:18:08 -0300

    Feat: Finish the logic of reverse_words, but it's not working in leetcode.  Changes to be committed: 	modified:   main.c 	modified:   reverse_words.c

--- leetcode/557._reverse_word_in_a_string_iii/c/main.c
+++ leetcode/557._reverse_word_in_a_string_iii/c/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char *reverseWords(char *s);
 
 int main(void)
 {
-	printf("%s", reverseWords("Let's take LeetCode contest"));
+	char	*result;
+	result = reverseWords("Let's take LeetCode contest");
+	printf("%s", result);
+	free(result);
 	return (0);
 }
--- leetcode/557._reverse_word_in_a_string_iii/c/reverse_words.c
+++ leetcode/557._reverse_word_in_a_string_iii/c/reverse_words.c
@@ -1,42 +1,38 @@
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-
-void getWords(char *s, char *new_word, size_t *i)
+size_t	getWords(char *s, char *new_word, size_t word_init, size_t word_start)
 {
-    size_t word_start;
-    size_t word_end;
+	size_t	 	word_end;
+	size_t	 	word_stop;
 
-    word_start = *i;
-    word_end = *i;
-	printf("foi");
-    while (s[*i] != ' ' || s[*i])
-    {
-        i++;
-        word_end++;
-    }
-	i++;
-	word_end++;
-	printf("%zu\n", word_end);
-    while (word_end > word_start)
-        *(new_word++) = s[word_end--];
+	word_stop = word_start;
+	while (s[word_start])
+	{
+		if (s[word_start] == ' ')
+			break ;
+		word_start++;
+		word_stop++;
+	}
+	word_end = word_stop + 1;
+	word_start = word_init;
+	while (word_stop > word_init)
+		new_word[word_start++] = s[--word_stop];
+	new_word[word_start] = ' ';
+	return (word_end);
 }
 
-char *reverseWords(char *s)
+char	*reverseWords(char *s)
 {
-    size_t  len;
-    size_t  i;
-    char    *new_word;
-    
-    len = strlen(s);
-    i = 0;
-    new_word = malloc(sizeof(char) * (len + 1));
-    while (*s)
-    {
-        getWords(s, new_word, &i);
-    }
-    new_word[i] = '\0';
-    return (new_word);
+	size_t	len;
+	size_t	i;
+	char	*new_word;
+
+	len = strlen(s);
+	i = 0;
+	new_word = malloc(sizeof(char) * (len + 10));
+	while (s[i])
+		i = getWords(s, new_word, i, i);
+	new_word[len] = '\0';
+	return (new_word);
 }
